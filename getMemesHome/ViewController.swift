4150813208644e793acaf45e5162f04e8670718a
Norhan Boghdadi <goodnorhan@gmail.com>
2022-03-12 18:58:35 +0200

    Remove tableView

--- getMemesHome/ViewController.swift
+++ getMemesHome/ViewController.swift
@@ -22,14 +22,7 @@ class ViewController: UIViewController {
         return label
     }()
     
-    private lazy var memesTableView: UITableView = {
-        let tableView = UITableView()
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.register(MemesTableViewCell.self, forCellReuseIdentifier: reuseIdentefier)
-        return tableView
-    }()
+
     
     
     override func viewDidLoad() {
@@ -37,7 +30,6 @@ class ViewController: UIViewController {
         viewModel = HomeViewModel(viewController: self)
         view.backgroundColor = .black
         setupTitle()
-        setupTableView()
     }
     //MARK: - Setting Title
     private func setupTitle() {
@@ -50,44 +42,16 @@ class ViewController: UIViewController {
         ])
     }
     
-    //MARK:- Setting the tableView
-    private func setupTableView() {
-        view.addSubview(memesTableView)
-        NSLayoutConstraint.activate([
-            memesTableView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
-            memesTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            memesTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            memesTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
-        ])
-    }
+
     
 
 
 }
 
-//MARK: - TableView Delegate and Data Source
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        viewModel?.numberOfMemes ?? 0
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = memesTableView.dequeueReusableCell(withIdentifier: reuseIdentefier) as! MemesTableViewCell
-        
-        guard let data = (viewModel?.data(for: indexPath)) else {return cell}
-        
-        cell.configure(for: data)
-        
-        return cell
-    }
-    
-    
-}
 
 // MARK: - Protocol Extension
 extension ViewController: NotifaiableController {
     func dataLoaded() {
-        memesTableView.reloadData()
     }
     
 }
